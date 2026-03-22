dfce1a19f0fbfcad80c32beb5d6995cd84cab4a1
suchita9335183105-ui <suchita9335183105@gmail.com>
2026-03-22 21:07:51 +0530

    Add files via upload

---
+++ list1ex.py
@@ -0,0 +1,9 @@
+#Program By: Suchita Yadav
+#Roll no.73
+#List Assingement No:
+
+#Create a list of 5 friend names. Print the first and last names using position and negative indexing
+
+friends = ["Rahul","Aman","Riya","Neha","Karan"]
+print("First name(positive index):",friends[0])
+print("Last name(negative index):",friends[-1])
